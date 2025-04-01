7fba5f0c6c600ba12f9a9decadda8bd7ed781801
Hugo Arvis <hugo.arvisft@gmail.com>
2025-04-01 14:29:26 +0200

    legal mentions page updated

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -37,7 +37,7 @@ if (!defined('ROOT_PATH')) {
                     <li><a href="<?php echo url(); ?>"><i class="fas fa-chevron-right me-2"></i>Accueil</a></li>
                     <li><a href="<?php echo url('offres'); ?>"><i class="fas fa-chevron-right me-2"></i>Offres de stage</a></li>
                     <li><a href="<?php echo url('entreprises'); ?>"><i class="fas fa-chevron-right me-2"></i>Entreprises</a></li>
-                    <li><a href="#"><i class="fas fa-chevron-right me-2"></i>Mentions légales</a></li>
+                    <li><a href="legalmention.php"><i class="fas fa-chevron-right me-2"></i>Mentions légales</a></li>
                 </ul>
             </div>
 
